364a9c0efde17fd9b573517d00b63413ee52fd99
RMI78 <romain.escrime@gmail.com>
2023-06-02 13:54:26 +0000

    Added test files for the SQL module

--- tests/integration/run.sh
+++ tests/integration/run.sh
@@ -13,7 +13,8 @@ test_mod_exec \
 test_mod_htaccess \
 test_mod_crlf \
 test_mod_xss \
-test_mod_xxe "
+test_mod_xxe \
+test_mod_sql "
 
 # Normalize trailing space for shell substitution
 if [[ ! -z "$TESTS" ]]; then
